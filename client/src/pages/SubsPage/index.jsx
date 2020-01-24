0c85f48ac14428808c5708a57d6b204b790577c3
edemskiy <qapple@ya.ru>
2020-01-24 19:55:16 +0300

    Add filter by record type

--- client/src/pages/SubsPage/index.jsx
+++ client/src/pages/SubsPage/index.jsx
@@ -4,9 +4,11 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 import "./SubsPage.scss";
 import { Album } from "../../components/Album";
+import { useRef } from "react";
 
 export function SubsPage() {
   const [albums, setAlbums] = useState([]);
+  const [activeTypeFilter, setActiveTypeFilter] = useState("");
   const { request, isLoading } = useRequest();
 
   const fetchAlbums = useCallback(() => {
@@ -18,9 +20,14 @@ export function SubsPage() {
     fetchAlbums();
   }, [fetchAlbums]);
 
-  useEffect(() => {
-    console.log(albums);
-  }, [albums]);
+  function onTypeChange(event) {
+    const activeButton = event.currentTarget.querySelector(".active");
+    if (activeButton) {
+      activeButton.classList.toggle("active");
+    }
+    event.target.classList.toggle("active");
+    setActiveTypeFilter(event.target.dataset.record_type);
+  }
 
   return (
     <>
@@ -29,11 +36,22 @@ export function SubsPage() {
           <CircularProgress size={60} color="inherit" />
         </div>
       ) : (
-        <div className="albums-container">
-          {albums.map(album => (
-            <Album key={album.id} album={album} />
-          ))}
-        </div>
+        <>
+          <div className="filter-buttons" onClick={onTypeChange}>
+            <button className="active" data-record_type="">
+              All
+            </button>
+            <button data-record_type="album">Albums</button>
+            <button data-record_type="single">Singles</button>
+          </div>
+          <div className="albums-container">
+            {albums
+              .filter(album => album.record_type.includes(activeTypeFilter))
+              .map(album => (
+                <Album key={album.id} album={album} />
+              ))}
+          </div>
+        </>
       )}
     </>
   );
